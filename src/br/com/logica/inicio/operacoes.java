9bf8fe2c439b3a067127e65a60b5d03da4efdc6d
NICOLAS GOMES MARTINS <nicolas.gmartins@senacsp.edu.br>
2023-03-21 16:22:27 -0300

    Inicio dos estudos em Java

---
+++ src/br/com/logica/inicio/operacoes.java
@@ -0,0 +1,33 @@
+package br.com.logica.inicio;
+
+import java.util.Scanner;
+
+public class operacoes {
+
+	public static void main(String[] args) {
+		//Vamos declarar 2 variáveis para realizar cálculos 
+        int n1;
+        int n2;
+        int soma, multiplicar, subtrair, dividir;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Digite um numero: ");
+        n1 = input.nextInt();
+        
+        System.out.println("Digite um numero ");
+        n2 = input.nextInt();
+        
+        soma =n1 + n2; 
+        multiplicar = n1 * n2;
+        subtrair = n1 - n2; 
+        dividir = n1 / n2; 
+
+	    System.out.println( "O resultado da soma é:"+ soma);
+	    System.out.println( "O resultado da multiplicação é:"+ multiplicar);
+	    System.out.println( "O resultado da subtração é:"+ subtrair);
+	    System.out.println( "O resultado da divisão é:"+ dividir);
+	    
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nicolas.gmartins
+ *
+ */
+module Logica {
+}
